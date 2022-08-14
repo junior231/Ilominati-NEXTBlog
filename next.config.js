ab4b2feaa745c42eaf30963c1a207363bf486a89
junior231 <ilocollins231@gmail.com>
2022-08-14 19:45:30 -0400

    Updated .env variables.

--- next.config.js
+++ next.config.js
@@ -1,32 +1,8 @@
 /** @type {import('next').NextConfig} */
-const {PHASE_DEVELOPMENT_SERVER} =  require('next/constants')
-const nextConfig = (phase) => {
- if (phase === PHASE_DEVELOPMENT_SERVER) {
-  return {
-    reactStrictMode: true,
-    swcMinify: true,
-    env: {
-      mongodb_username: 'ilo',
-      mongodb_password: 'nextblog',
-      mongodb_clusterName: 'cluster0',
-      mongodb_databaseName: 'blog',
-      mongodb_collection: 'dev-messages'
-    }
-  }
- }
 
-  // return {
-  //   reactStrictMode: true,
-  //   swcMinify: true,
-  //   env: {
-  //     mongodb_username: 'ilo',
-  //     mongodb_password: 'nextblog',
-  //     mongodb_clusterName: 'cluster0',
-  //     mongodb_databaseName: 'blog',
-  //     mongodb_collection: 'messages'
-  //   }
-  // }
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+};
 
-}
-
-module.exports = nextConfig
+module.exports = nextConfig;
